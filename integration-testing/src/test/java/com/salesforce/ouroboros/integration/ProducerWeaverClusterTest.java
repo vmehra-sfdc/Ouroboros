63384384e87ae6d9c9f3a05035a7a2fdbb0057b8
Hal Hildebrand <hal.hildebrand@gmail.com>
2012-04-05 19:12:05 -0700

    reduce batch number

--- integration-testing/src/test/java/com/salesforce/ouroboros/integration/ProducerWeaverClusterTest.java
+++ integration-testing/src/test/java/com/salesforce/ouroboros/integration/ProducerWeaverClusterTest.java
@@ -208,7 +208,7 @@ public class ProducerWeaverClusterTest {
 
     private static final int     BATCH_SIZE    = 10;
 
-    private static final int     BATCH_COUNT   = 200;
+    private static final int     BATCH_COUNT   = 100;
 
     private static final int     CHANNEL_COUNT = 25;
 
